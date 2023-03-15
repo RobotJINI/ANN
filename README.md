1e6b70de16ce8506b250e0978f9cd29118297745
“RobotJINI” <gamache88@gmail.com>
2023-03-14 22:21:45 -0400

    completed example of ann

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-# Retention Radar
-Artificial Neural Network binary classification example.
+# RetentionRadar
+RetentionRadar is a Python class that trains an Artificial Neural Network (ANN) model to predict whether a bank customer will leave the bank or not. The model is trained on the "Churn_Modelling.csv" dataset and uses features such as the credit score, gender, age, balance, and estimated salary of the customer to make predictions.
 
-Assesses bank customer churn based on 10000 samples in Churn_Modelling.csv
+## Requirements
+* pandas
+* scikit-learn
+* tensorflow
+
+## Installation
+You can install the required dependencies using pip:
+
+```bash
+pip install pandas scikit-learn tensorflow
+```
+
+## Usage
+To use RetentionRadar, you can create an instance of the class and pass in the filename of the dataset:
+
+```python
+from RetentionRadar import RetentionRadar
+
+rr = RetentionRadar('Churn_Modelling.csv')
+```
+
+The RetentionRadar object will automatically load and preprocess the dataset, create an ANN model, and train it on the training data.
+
+To make predictions for new customers, you can call the predict_leave_bank() method of the RetentionRadar object and pass in a 2D array of new observations:
+
+```python
+prediction = rr.predict_leave_bank([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])
+```
+
+This will return a boolean value indicating whether the customer is likely to leave the bank or not.
+
+To evaluate the performance of the model, you can call the test() method of the RetentionRadar object:
+
+```python
+rr.test()
+```
+
+This will print the confusion matrix and accuracy score of the model on the test data.
--- retention_radar.py
+++ retention_radar.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from tensorflow.keras.models import load_model
 import os
 import logging
 
@@ -27,63 +29,96 @@ def import_tensorflow():
     return tf
 tf = import_tensorflow()  
 
-def load_train_test(file_name):
-    dataset = pd.read_csv(file_name)
+class RetentionRadar:
+    def __init__(self, file_name):
+        self.load_train_test(file_name)
+        self.create_ann()
     
-    #Start with credit score (index 3)
-    features = dataset.iloc[:, 3:-1].values
-    results = dataset.iloc[:, -1].values
+    def load_train_test(self, file_name):
+        self._dataset = pd.read_csv(file_name)
+        
+        #Start with credit score (index 3)
+        features = self._dataset.iloc[:, 3:-1].values
+        results = self._dataset.iloc[:, -1].values
+        
+        logging.debug(f'features:\n{features}')
+        logging.debug(f'dep_variable:\n{results}')
+        
+        #Label encoding gender column
+        le = LabelEncoder()
+        features[:, 2] = le.fit_transform(features[:, 2])
+        logging.debug(f'features label encoding gender column:\n{features}')
+        
+        #One hot encoding on Geography column
+        ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+        features = np.array(ct.fit_transform(features))
+        logging.debug(f'features one hot encoding on geography column:\n{features}')
+        
+        # splitting dataset into training and test set
+        self._features_train, self._features_test, self._results_train, self._results_test = train_test_split(features, results, test_size = 0.2, random_state = 0)
+        
+        # feature scaling
+        self._sc = StandardScaler()
+        self._features_train = self._sc.fit_transform(self._features_train)
+        self._features_test = self._sc.transform(self._features_test)
     
-    logging.debug(f'features:\n{features}')
-    logging.debug(f'dep_variable:\n{results}')
+    def create_ann(self):
+        # Initializing the ANN
+        self._ann = tf.keras.models.Sequential()
+        
+        # Adding the input layer and the first hidden layer
+        self._ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+        
+        # Adding the second hidden layer
+        self._ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+        
+        # Adding the output layer
+        self._ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+        
+        return self._ann
     
-    #Label encoding gender column
-    le = LabelEncoder()
-    features[:, 2] = le.fit_transform(features[:, 2])
-    logging.debug(f'features label encoding gender column:\n{features}')
+    def train(self):
+        # Compiling the ANN
+        self._ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+        
+        # Training the ANN on the Training set
+        self._ann.fit(self._features_train, self._results_train, batch_size = 32, epochs = 100)
+        
+    def predict_leave_bank(self, new_observations):
+        return (self._ann.predict(self._sc.transform(new_observations)) > 0.5)
     
-    #One hot encoding on Geography column
-    ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-    features = np.array(ct.fit_transform(features))
-    logging.debug(f'features one hot encoding on geography column:\n{features}')
-    
-    # splitting dataset into training and test set
-    features_train, features_test, results_train, results_test = train_test_split(features, results, test_size = 0.2, random_state = 0)
-    
-    # feature scaling
-    sc = StandardScaler()
-    features_train = sc.fit_transform(features_train)
-    features_test = sc.transform(features_test)
-    
-    return (features_train, features_test, results_train, results_test)
-
-def create_ann():
-    # Initializing the ANN
-    ann = tf.keras.models.Sequential()
-    
-    # Adding the input layer and the first hidden layer
-    ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-    
-    # Adding the second hidden layer
-    ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-    
-    # Adding the output layer
-    ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-    
-    return ann
-
-def train(ann):
-    # Compiling the ANN
-    ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-    
-    # Training the ANN on the Training set
-    ann.fit(features_train, results_train, batch_size = 32, epochs = 100)
+    def test(self):
+        pred_test = self._ann.predict(self._features_test)
+        pred_test = (pred_test > 0.5)
+        
+        cm = confusion_matrix(self._results_test, pred_test)
+        logging.info(f'confusion matrix:\n{cm}')
+        acc_score = accuracy_score(self._results_test, pred_test)
+        logging.info(f'accuracy score:\n{acc_score}')
 
 
 if __name__ == "__main__":
     logging.basicConfig(format='\n%(message)s\n', level=logging.DEBUG)
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     logging.debug(tf.__version__)
     
-    features_train, features_test, results_train, results_test = load_train_test('Churn_Modelling.csv')
-    create_ann()
+    rr = RetentionRadar('Churn_Modelling.csv')
+    rr.train()
+    
+    '''
+    Geography: France
+    Credit Score: 600
+    Gender: Male
+    Age: 40 years old
+    Tenure: 3 years
+    Balance: $ 60000
+    Number of Products: 2
+    Does this customer have a credit card? Yes
+    Is this customer an Active Member: Yes
+    Estimated Salary: $ 50000
+    '''
+    prediction = rr.predict_leave_bank([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])
+    logging.info(f'Will customer leave bank? {prediction}')
+    
+    rr.test()
+    
+    
